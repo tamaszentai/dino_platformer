3854ddc2a94d82b1217a4215a2ef7a50619f0e76
Tamás Zentai <zentaitamas@gmail.com>
2023-11-22 20:37:17 +0000

    generate platforms first, then player

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -6,28 +6,28 @@ export default class Game {
     width: number;
     height: number;
     gravity: number;
-    player: Player;
     platforms: Platform[];
+    player: Player;
     inputHandler: InputHandler;
     constructor(width: number, height: number, playerName: string) {
         this.width = width;
         this.height = height;
         this.gravity = 0.5;
+        this.platforms = [new Platform(this)]
+        // for (let i = 0; i < 5; i++) {
+        //     this.platforms.push(new Platform(this));
+        // }
         this.player = new Player(this, playerName);
-        this.platforms = []
         this.inputHandler = new InputHandler(this);
-
-        for (let i = 0; i < 5; i++) {
-            this.platforms.push(new Platform(this));
-        }
     }
     update() {
         this.player.update();
     }
     draw(context: CanvasRenderingContext2D){
-        this.player.draw(context);
         this.platforms.forEach(platform => {
             platform.draw(context);
         })
+        this.player.draw(context);
+
     }
 }
